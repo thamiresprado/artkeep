747409a2a81217d6af5cdf5815eb721c74a97b5d
Thamires Prado <thamiresmelo04@gmail.com>
2023-10-01 16:31:51 -0300

    art details: carrousel done

--- testeartkeep/lib/pages/art-details.dart
+++ testeartkeep/lib/pages/art-details.dart
@@ -29,7 +29,8 @@ class _DetailsPageState extends State<DetailsPage> {
                     width: double.infinity,
                   ),
                   Container(
-                    color: Colors.black.withOpacity(0.7), //background
+                    color: Colors.black
+                        .withOpacity(0.7), //background
                   ),
                 ],
               ),
@@ -42,9 +43,10 @@ class _DetailsPageState extends State<DetailsPage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
+                  // Ícone à esquerda
                   GestureDetector(
                     onTap: () {
-                      widget.navigator.pop();
+                      widget.navigator.pop(); // Voltar para a página anterior
                     },
                     child: const Icon(
                       Icons.arrow_back,
@@ -52,6 +54,7 @@ class _DetailsPageState extends State<DetailsPage> {
                       color: Colors.white,
                     ),
                   ),
+                  // Ícone à direita
                   const Icon(
                     Icons.star_border,
                     size: 32.0,
@@ -60,25 +63,111 @@ class _DetailsPageState extends State<DetailsPage> {
                 ],
               ),
             ),
+            // Container branco com bordas arredondadas e cor de borda rosa
             Positioned(
               left: 20.0,
               right: 20.0,
-              bottom: MediaQuery.of(context).size.height * 0.67 - 100.0,
+              bottom: MediaQuery.of(context).size.height * 0.72 - 100.0,
               height: 180.0,
               child: Container(
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.circular(4.0),
+                  borderRadius: BorderRadius.circular(4.0), // Bordas de 16px
                   border: Border.all(
                     color: Colors.grey,
                     width: 0.3,
                   ),
                 ),
+                padding: EdgeInsets.all(16.0), // Preenchimento interno de 16px
+                child: Row(
+                  children: [
+                    Container(
+                      width: 150,
+                      height: 150,
+                      margin: const EdgeInsets.only(right: 12.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4.0), // Bordas de 16px
+                    ),
+                      child: Image.asset(
+                        'lib/images/foryou-1.png',
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      ),
+                    ),
+
+                    const Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment:MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Annunciation Triptych (Merode Altarpiece)',
+                            style: TextStyle(
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.w900,
+                              height: 1.0,
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            
-            //o resto do conteudo aqui
 
+
+            Align( //carrossel
+              alignment: const Alignment(0, 0.15),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 250,
+                        height: 250,
+                        margin: const EdgeInsets.only(left: 20),
+                        child: Image.asset(
+                          'lib/images/art1.png',
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
+                      Container(
+                        width: 250,
+                        height: 250,
+                        margin: const EdgeInsets.only(left: 8),
+                        child: Image.asset(
+                          'lib/images/art2.png',
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
+                      Container(
+                        width: 250,
+                        height: 250,
+                        margin: const EdgeInsets.only(left: 8),
+                        child: Image.asset(
+                          'lib/images/art3.png',
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
+                      Container(
+                        width: 250,
+                        height: 250,
+                        margin: const EdgeInsets.only(left: 8, right: 20),
+                        child: Image.asset(
+                          'lib/images/art4.png',
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
           ],
         ),
       ),
